2a0e5b71cb8ce261b88ab82c495753a8416f7cd4
Prachi Nayakal <121372605+prachinayakal@users.noreply.github.com>
2024-10-16 21:59:30 +0530

    Add files via upload

---
+++ oper.py
@@ -0,0 +1,88 @@
+#learn different operators in Python
+
+# 1. Arithmetic
+# Arithmetic operator contains addition(+), subtraction(-),
+# multiplication(*), division(/), modulus(%),exponentiation(**), floor(//)
+
+# 1.1 Addition(+)
+a=10
+b=5
+print(a+b) #15
+
+# 1.2 Subtraction(-)
+a=10
+b=5
+print(a-b) #5
+
+# 1.3 Multiplication(*)
+a=10
+b=2
+print(a*b) #20
+
+# 1.4 Division(/)
+a=10
+b=5
+print(a/b) #2.0
+
+# 1.5 modulus(%)
+a=53
+b=5
+print(a%b) #3
+
+# 1.6 exponentiation(**)
+a=2
+b=3
+print(a**b) #8
+
+# 1.7 floor(//)
+a=15
+b=5
+print(a//b) #3
+
+
+# 2. Comparision
+#Comparision operator is used to compare two values and write true or false
+#comparision operator contains ==(equal to), !=(not equal to), >(greater than), 
+# <(less than), >=(greater than or equal to), <=(less than or equal to)
+
+# 2.1 ==(equal to)
+print(5==5)
+
+# 2.2 !=(not equal to)
+print(5!=6)
+
+# 2.3 >(greater than)
+print(8>5)
+ 
+# 2.4 <(less than)
+print(4<45)
+
+# 2.5 >=(greater than or equal to)
+print(15>=10)
+
+# 2.6 <=(less than or equal to)
+print(10<=10)
+
+
+
+# 3. Logical operator
+# combines multiple conditional expressions
+# it contains and, or, not
+
+#3.1 and
+print (5<48) and (8<6)
+
+#3.2 or
+print (5>10) or (8>6)
+
+#3.3 not
+not(10>5)
+
+
+# 4. Assignment
+# Assignment operator is used to assign value to the operator
+
+# 4.1 ==(assign)
+x=10
+print(x)
+
